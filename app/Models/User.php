3a0b079ea8a27ccd836640eb13267cfef23815c7
chivichi3143 <chivichi3143@gmail.com>
2026-05-04 00:59:16 -0600

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -45,5 +48,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        $allowedAdmins = [
+            'banditcodex@gmail.com',
+            'admin@eltorogoz.com',
+        ];
+
+        return in_array(Str::lower((string) $this->email), $allowedAdmins, true);
     }
 }
